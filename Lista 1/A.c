1bd63aba8b081be56908f343c0a449468cbc51e1
Lucas Emanuel <131834616+llucasEmanuel@users.noreply.github.com>
2023-11-02 12:59:29 -0300

    Update A.c

--- Lista 1/A.c
+++ Lista 1/A.c
@@ -1,72 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void quickSort(int *array, int l, int r);
-int HoarePartition(int *array, int l, int r);
+void mergeSort(long long int *array, int l , int r);
+void merge(long long int *array, int l , int r);
 
 int main() {
     int n;
     scanf("%d", &n);
-    int precos[n];
+    long long int *precos = (long long int *) malloc(n * sizeof(long long int));
     for (int i = 0; i < n; i++) {
-        scanf("%d", &precos[i]);
+        scanf("%lld", &precos[i]);
     }
-    quickSort(precos, 0, n-1);
+    mergeSort(precos, 0, n-1); // ordena de forma decrescente
 
     int m;
     scanf("%d", &m);
-    int qtdBarrasCupom[m];
+    long long int *somas = (long long int *) malloc((n+1) * sizeof(long long int));
+    somas[0] = 0;
+    for (int i = 0; i < n; i++) {
+        somas[i+1] = precos[i] + somas[i];
+    }
+    int qtdBarrasCupom;
     for (int i = 0; i < m; i++) {
-        scanf("%d", &qtdBarrasCupom[i]);
+        scanf("%d", &qtdBarrasCupom);
+        printf("%lld\n", somas[n] - precos[qtdBarrasCupom-1]);
     }
 
-    int soma = 0;
-    for (int i = 0; i < m; i++) {
-        int j;
-        for (j = 0; j < qtdBarrasCupom[i] - 1; j++) {
-            soma += precos[j];
-        }
-        j++;
-        while (j < n) {
-            soma += precos[j];
-            j++;
-        }
-        printf("%d\n", soma);
-        soma = 0;
-    }
+    free(somas);
+    free(precos);
     return 0;
 }
 
-void quickSort(int *array, int l, int r) {
+void mergeSort(long long int *array, int l , int r) {
     if (l < r) {
-        int p = HoarePartition(array, l, r);
-        quickSort(array, l, p-1);
-        quickSort(array, p+1, r); 
+        int m = (l+r)/2;
+        mergeSort(array, l, m);
+        mergeSort(array, m+1, r);
+        merge(array, l, r);
     }
 }
-
-int HoarePartition(int *array, int l, int r) {
-    int pivot = array[l];
-    int i = l;
-    int j = r + 1;
-    int temp;
-    do {
-        do {
-            i++;
-        } while (array[i] > pivot && i < r);
-        do {
-            j--;
-        } while (array[j] < pivot);
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    } while (i < j);
-
-    temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-
-    temp = array[l];
-    array[l] = array[j];
-    array[j] = temp;
-    return j;
+void merge(long long int *array, int l , int r) {
+    int m = (l+r)/2;
+    int i1 = l;
+    int i2 = m+1;
+    int *temp = (int *) malloc((r+1) * sizeof(int));
+    for (int i = l; i <= r; i++) {
+        temp[i] = array[i];
+    }
+    for (int curr = l; curr <= r; curr++) {
+        if (i1 == m+1) {
+            array[curr] = temp[i2++];
+        }
+        else if (i2 > r) {
+            array[curr] = temp[i1++];
+        }
+        else if (temp[i1] <= temp[i2]) {
+            array[curr] = temp[i2++];
+        }
+        else {
+            array[curr] = temp[i1++];
+        }
+    }
+    free(temp);
 }
