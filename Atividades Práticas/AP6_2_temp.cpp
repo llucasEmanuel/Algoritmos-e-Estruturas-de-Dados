7481231d88aed22fd10d4496f4414fdcf493b79a
Lucas Emanuel <131834616+llucasEmanuel@users.noreply.github.com>
2023-11-30 15:53:43 -0300

    Update AP6_2_temp.cpp

--- Atividades Práticas/AP6_2_temp.cpp
+++ Atividades Práticas/AP6_2_temp.cpp
@@ -1,74 +1,113 @@
 #include <iostream>
+#include <vector>
 #define endl '\n'
 
 using namespace std;
 
-template <typename T> class table_node {
+class Cell {
 public:
+    string key;
     bool empty;
-    T value;
+    Cell() {
+        this->empty = true; // inicializa todos como empty
+    }
 };
 
-class Dictionary {
+class HashTable{
 private:
-    int table_size; // m
-    table_node<string> *hash_table = new table_node<string>[this->table_size];
-    // desalocar no destrutor
-    int hash(string str) { // fold
+    vector<Cell> table;
+    int hash(string str) {
         int sum = 0;
-        for (int i = 0; i < str.size(); i++) {
-            sum += str[i];
+        for (char &i : str) {
+            sum += i;
         }
-        return abs(sum) % (this->table_size);
+        return abs(sum) % this->table.size();
     }
-    int linearProbing(int hash) { 
-        int first_hash = hash;
-        do{ 
-            hash++; // incrementa primeiro (garante q n caia no if na primeira iteracao)
-            if (hash == first_hash) {
-                return -1;
-            }
-        } while (!this->hash_table[hash].empty);
-        return hash;
+    int probe(int &i) { // retorna o step em relacao ao calculado pela função hash
+        return i++;
     }
 public:
-    void setTableSize(int size) {
-        this->table_size = size;
+    HashTable(int m) {
+        this->table.resize(m);
     }
-    void add(string str) {
-        int ind = hash(str);
-        while (!this->hash_table[ind].empty) {
-            ind = linearProbing(ind);
+    void add(string key) {
+        int ind = hash(key);
+        int first_ind = ind;
+        int i = 0;
+        while (!this->table[ind].empty && i < this->table.size()) {
+            // calculo do step em relacao ao primeiro ind da funcao hash
+            ind = (first_ind + probe(i)) % this->table.size();
         }
-        this->hash_table[ind].value = str;
-        this->hash_table[ind].empty = false;
-    }
-    void remove(string str) {
-        int ind = hash(str);
-        if (this->hash_table[ind].value == str) {
-            this->hash_table[ind].empty = true;
-            this->hash_table[ind].value = "";
-            //usa a flag para deixar o espaco disponivel
+
+        if (this->table[ind].empty) {
+            this->table[ind].key = key;
+            this->table[ind].empty = false;
+            //cout << "Adicionado no indice " << ind << endl;
         }
         else {
-        
+            cout << "Tabela cheia" << endl;
         }
     }
-    string search(string str) {
-        int ind = hash(str);
-        if (this->hash_table[ind].value == str) {
-            return this->hash_table[ind].value;
+    void rmv(string key) {
+        int ind = hash(key);
+        int first_ind = ind;
+        int i = 0;
+        while (this->table[ind].key != key && i < this->table.size()) {
+            ind = (first_ind + probe(i)) % this->table.size();
         }
-        else { //linear probing
-        // se n tivesse o elemento seria um loop infinito
-        // Lista tabela[5]
+
+        if (this->table[ind].key == key && !this->table[ind].empty) {
+            this->table[ind].empty = true;
+            this->table[ind].key = "";
         }
+        else {
+            cout << "Chave nao encontrada" << endl;
+        }
+    }
+    int sch(string key) {
+        int ind = hash(key);
+        int first_ind = ind;
+        int i = 0;
+        while (this->table[ind].key != key && i < this->table.size()) {
+            ind = (first_ind + probe(i)) % this->table.size();
+        }
+        cout << key << ' ';
+        if (this->table[ind].key == key && !this->table[ind].empty) {
+            cout << ind << endl;
+        }
+        else {
+            cout << -1 << endl;
+        }
+        return ind;
     }
 };
 
 int main() {
 
+    int m;
+    cin >> m;
+    HashTable table(m);
 
+    string comando;
+    do {
+        cin >> comando;
+        if (comando == "add") {
+            string key;
+            cin >> key;
+            table.add(key);
+        }
+        else if (comando == "sch") {
+            string key;
+            cin >> key;
+            table.sch(key);
+        }
+        else if (comando == "rmv") {
+            string key;
+            cin >> key;
+            table.rmv(key);
+        }
+
+    } while (comando != "fim");
 
     return 0;
 }
